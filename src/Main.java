70f30b79f56fe2be27284a866b0a19d8c0db633c
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-01-24 13:04:20 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        var box = 5;
-        System.out.println(box);
-        box = box + 2;
-        System.out.println(box);
-        box = box - 3;
-        System.out.println(box);
+        task1();
+        task2();
+        task3();
+    }
 
+    public static void task1() {
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
     }
 }
