4e19fa09b55d8cab1ae8adf949712275c6c3f4a8
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-01-24 13:48:47 +0300

    Задача 4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
+
     }
 
     public static void task1() {
@@ -40,5 +43,18 @@ public class Main {
         var paper = 763789 + 4;
         paper = paper - 7639;
         System.out.println(paper);
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
     }
 }
